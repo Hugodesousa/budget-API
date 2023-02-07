d12fcc317dc98fd3ea158783bf0b40b2cab2a57e
HugoWSL <hugodesousa.16@gmail.com>
2023-02-07 00:07:13 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 
 import swaggerFile from '../swagger_output.json';
-// import fs from 'fs';
 
 import express from 'express';
 import 'express-async-errors';
@@ -9,16 +8,8 @@ import userRoutes from './Routes/Users.routes';
 import productsRoutes from './Routes/Products.routes';
 import budgetRoutes from './Routes/Budget.routes';
 import { GenericError } from './err/GenericError';
-import cors from 'cors';
-
-// let swaggerFile;
-// fs.readFile('./swagger_output.json', 'utf-8', (err, data) => {
-//   if (err) throw err;
-//   swaggerFile = JSON.parse(data);
-// });
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
 app.use(productsRoutes);
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import app from './app';
 import dotenv from 'dotenv' 
 dotenv.config()
 
+import cors from 'cors';
+
 const PORT = process.env.PORT || 3001;
+app.use(cors());
 
 app.get('/', (_req, res) => {
   res.status(200).send({ status: 'app is running.' });
