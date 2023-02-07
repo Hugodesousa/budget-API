9c3527a94669b5717e01aef0bc5885b0993a7048
HugoWSL <hugodesousa.16@gmail.com>
2023-02-06 23:33:29 -0300

    correção nos scrips "build e start" e configuração "resolveJsonModule

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const swaggerFile = require('../swagger_output.json');
+
+import swaggerFile from '../swagger_output.json';
+// import fs from 'fs';
+
 import express from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express'
@@ -9,13 +11,19 @@ import budgetRoutes from './Routes/Budget.routes';
 import { GenericError } from './err/GenericError';
 import cors from 'cors';
 
+// let swaggerFile;
+// fs.readFile('./swagger_output.json', 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   swaggerFile = JSON.parse(data);
+// });
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
 app.use(productsRoutes);
 app.use(budgetRoutes);
-// app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(GenericError);
 
 export default app;
